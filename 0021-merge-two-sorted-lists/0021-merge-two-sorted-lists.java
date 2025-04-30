9c50e35a65474a90c44e842e64b62cc73055bebf
Mansoor Shaik <133763110+smansoor481@users.noreply.github.com>
2025-04-30 21:50:32 +0530

    Time: 0 ms (100%), Space: 42.9 MB (14.41%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,7 +10,6 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode head1, ListNode head2) {
-
         if(head1 == null || head2 == null)
         {
             return head1 == null ? head2 : head1;
@@ -21,11 +20,11 @@ class Solution {
             head1.next = mergeTwoLists(head1.next, head2);
             return head1;
         }
+
         else
         {
             head2.next = mergeTwoLists(head1, head2.next);
             return head2;
         }
-        
     }
 }
