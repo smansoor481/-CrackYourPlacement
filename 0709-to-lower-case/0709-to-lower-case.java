35933dad85da714edbfec6b346182b436251b97e
Mansoor Shaik <133763110+smansoor481@users.noreply.github.com>
2025-11-24 08:07:02 +0530

    Time: 1 ms (17.55%), Space: 43.2 MB (6.17%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,6 +1,21 @@
 class Solution {
     public String toLowerCase(String s) {
-        String str = s.toLowerCase();
-        return str;
+        // String str = s.toLowerCase();
+        // return str;
+
+
+        StringBuilder lowercase = new StringBuilder();
+        for(char c : s.toCharArray())
+        {
+            if(c >= 'A' && c <= 'Z')
+            {
+                lowercase.append((char)(c + 32));
+            }
+            else
+            {
+                lowercase.append(c);
+            }
+        }
+        return lowercase.toString();
     }
 }
