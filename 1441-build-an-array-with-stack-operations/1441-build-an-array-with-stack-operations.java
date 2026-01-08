79701569774fabc108cf0287946aff4c07b17b94
Mansoor Shaik <133763110+smansoor481@users.noreply.github.com>
2026-01-08 09:07:36 +0530

    Time: 0 ms (100%), Space: 44.4 MB (14.46%) - LeetHub

---
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        
+        int tIndex = 0;
+        List<String> li = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++)
+        {
+
+            li.add("Push");
+
+            if (tIndex < target.length && i == target[tIndex]) 
+            {
+                tIndex++;
+            } 
+            else
+            {
+                li.add("Pop");
+            }
+
+            if (tIndex == target.length) 
+            {
+                break;
+            }
+        }
+
+        return li;
+    }
+}
